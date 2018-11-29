a048e518ff92ebed94665224225d1664403dbd56
Christopher Enytc <chris@enytc.com>
2018-11-29 02:21:10 -0200

    Update main

--- hangman_game/__main__.py
+++ hangman_game/__main__.py
@@ -4,14 +4,20 @@
 from .engine import start_game
 from .helpers import print_message
 
-# Start the game
-try:
-    start_game(True)
-# Catch exceptions
-except Exception as e:
-    print_message("An error has occurred during the game: %s" % e)
-except KeyboardInterrupt:
-    print("")
-    print("\n    Game closed. See you later :)")
-    print("")
-    exit(0)
+
+def main():
+    # Start the game
+    try:
+        start_game(True)
+    # Catch exceptions
+    except Exception as e:
+        print_message("An error has occurred during the game: %s" % e)
+    except KeyboardInterrupt:
+        print("")
+        print("\n    Game closed. See you later :)")
+        print("")
+        exit(0)
+
+
+# Call main
+main()
