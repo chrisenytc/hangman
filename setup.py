b9bd5dfac4182a5d099b8bd29bec911b4c55464e
Christopher Enytc <chris@enytc.com>
2018-11-29 02:50:02 -0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hangman-game",
-    version="1.0.5",
+    version="1.0.6",
     author="Christopher Enytc",
     author_email="chris@enytc.com",
     description="A CLI game built with Python",
