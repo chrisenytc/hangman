dcd4fbea0fb4aa99298b79a8328ed4b1d6406645
Christopher Enytc <chris@enytc.com>
2018-11-29 00:56:47 -0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hangman_cli",
+    version="1.0.0",
+    author="Christopher Enytc",
+    author_email="chris@enytc.com",
+    description="A CLI game built with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrisenytc/hangman",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'hangman = hangman_cli.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
